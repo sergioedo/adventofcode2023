72a08cd1b7f616d81d4ab4d631bc9b44f866b023
sergioedo <sergio.edo@gmail.com>
2023-12-02 13:30:49 +0100

    day 2 - part 2

--- day2/index.js
+++ day2/index.js
@@ -12,22 +12,48 @@ const isGamePossible = (cubesShows, bagCubes) => {
     })
 }
 
+const readGame = (line) => {
+    const gameId = Number(line.split(':')[0].split(' ')[1])
+    const cubesShows = line.split(':')[1].split(';').map(cubesShow => {
+        return cubesShow.split(',').reduce((prev, line) => {
+            const quantity = Number(line.trim().split(' ')[0].trim())
+            const color = line.trim().split(' ')[1].trim()
+            return {
+                ...prev,
+                [color]: quantity
+            }
+        }, {})
+    })
+    return { gameId, cubesShows }
+}
+
 export const possibleGames = (inputFile) => {
     const input = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
 
-    return input.reduce((prev, curr, currIdx, arr) => {
-        const gameId = Number(curr.split(':')[0].split(' ')[1])
-        const cubesShows = curr.split(':')[1].split(';').map(cubesShow => {
-            return cubesShow.split(',').reduce((prev, curr) => {
-                const quantity = Number(curr.trim().split(' ')[0].trim())
-                const color = curr.trim().split(' ')[1].trim()
-                return {
-                    ...prev,
-                    [color]: quantity
-                }
-            }, {})
-        })
+    return input.reduce((counter, currLine) => {
+        const { gameId, cubesShows } = readGame(currLine)
         const isPossible = isGamePossible(cubesShows, bagCubes);
-        return isPossible ? prev + gameId : prev
+        return isPossible ? counter + gameId : counter
+    }, 0)
+}
+
+const minimumGameCubes = (cubesShows) => {
+    return cubesShows.reduce((prev, curr) => {
+        return {
+            ...prev,
+            red: Math.max(prev.red, curr.red || 0),
+            green: Math.max(prev.green, curr.green || 0),
+            blue: Math.max(prev.blue, curr.blue || 0),
+        }
+    }, { red: 0, green: 0, blue: 0 })
+}
+
+export const powerMinimumGameCubes = (inputFile) => {
+    const input = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
+
+    return input.reduce((counter, currLine) => {
+        const { gameId, cubesShows } = readGame(currLine)
+        const minGamecubes = minimumGameCubes(cubesShows);
+        return counter + (minGamecubes.red * minGamecubes.blue * minGamecubes.green)
     }, 0)
 }
--- day2/index.test.js
+++ day2/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { possibleGames } from 'day2'
+import { possibleGames, powerMinimumGameCubes } from 'day2'
 import path from 'path'
 
 test('Test possibleGames sample input', () => {
@@ -14,4 +14,18 @@ test('Test possibleGames input', () => {
     const expectedOutput = 2348
 
     expect(possibleGames(inputFile)).toBe(expectedOutput)
+})
+
+test('Test powerMinimumGameCubes sample input', () => {
+    const inputFile = path.join('day2', 'input.sample.txt')
+    const expectedOutput = 2286
+
+    expect(powerMinimumGameCubes(inputFile)).toBe(expectedOutput)
+})
+
+test('Test powerMinimumGameCubes input', () => {
+    const inputFile = path.join('day2', 'input.txt')
+    const expectedOutput = 76008
+
+    expect(powerMinimumGameCubes(inputFile)).toBe(expectedOutput)
 })
