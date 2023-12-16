abb77c6d0f456f476d70ce145fb323a4dfd5e1b2
sergioedo <sergio.edo@gmail.com>
2023-12-16 08:38:46 +0100

    day 3 - part 2

--- day3/index.js
+++ day3/index.js
@@ -2,20 +2,24 @@ import fs from 'fs'
 
 const isDigit = (char) => char && char.match(/[0-9]/)
 const isSimbol = (char) => char && char !== '.' && !isDigit(char)
+const isGearSimbol = (char) => char && char === '*'
 const isNumber = (text) => text.match(/[0-9]+/)
 const toNumber = (text) => Number(text.match(/[0-9]+/)[0])
 
-const hasAdjacentSymbol = (grid, row, col) => {
-    if (isSimbol(grid?.[row - 1]?.[col - 1])) return true
-    if (isSimbol(grid?.[row - 1]?.[col + 0])) return true
-    if (isSimbol(grid?.[row - 1]?.[col + 1])) return true
-    if (isSimbol(grid?.[row + 0]?.[col - 1])) return true
-    if (isSimbol(grid?.[row + 0]?.[col + 1])) return true
-    if (isSimbol(grid?.[row + 1]?.[col - 1])) return true
-    if (isSimbol(grid?.[row + 1]?.[col + 0])) return true
-    if (isSimbol(grid?.[row + 1]?.[col + 1])) return true
-    return false
+const getAdjacentSymbolsIndex = (grid, row, col, isSimbol) => {
+    const adjacentsSimbols = []
+    if (isSimbol(grid?.[row - 1]?.[col - 1])) adjacentsSimbols.push([row - 1, col - 1])
+    if (isSimbol(grid?.[row - 1]?.[col + 0])) adjacentsSimbols.push([row - 1, col + 0])
+    if (isSimbol(grid?.[row - 1]?.[col + 1])) adjacentsSimbols.push([row - 1, col + 1])
+    if (isSimbol(grid?.[row + 0]?.[col - 1])) adjacentsSimbols.push([row + 0, col - 1])
+    if (isSimbol(grid?.[row + 0]?.[col + 1])) adjacentsSimbols.push([row + 0, col + 1])
+    if (isSimbol(grid?.[row + 1]?.[col - 1])) adjacentsSimbols.push([row + 1, col - 1])
+    if (isSimbol(grid?.[row + 1]?.[col + 0])) adjacentsSimbols.push([row + 1, col + 0])
+    if (isSimbol(grid?.[row + 1]?.[col + 1])) adjacentsSimbols.push([row + 1, col + 1])
+    return adjacentsSimbols
 }
+
+const hasAdjacentSymbol = (grid, row, col) => (getAdjacentSymbolsIndex(grid, row, col, isSimbol).length > 0)
 
 export const sumPartNumbers = (inputFile) => {
     const schematic = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n').map(line => line.split(''))
@@ -46,4 +50,61 @@ export const sumPartNumbers = (inputFile) => {
         isValidPart = false
     })
     return validParts.reduce((counter, validPart) => counter + validPart, 0)
+}
+
+const getAdjacentSimbolsIndexByRowRange = (grid, row, startCol, endCol, isSimbol) => {
+    const adjacentsSimbols = []
+    for (let col = startCol; col <= endCol; col++) {
+        adjacentsSimbols.push(...getAdjacentSymbolsIndex(grid, row, col, isSimbol).map(idx => idx.join('-')))
+    }
+    return [...new Set(adjacentsSimbols)]
+}
+
+export const sumGearRatios = (inputFile) => {
+    const schematic = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n').map(line => line.split(''))
+
+    const gearsNumbers = {}  // store gears by index "row-col", and value is its adjacents numbers as an array
+    schematic.map((row, rowIdx) => {
+        const partDigits = []
+        let isValidPart = false
+        row.map((col, colIdx) => {
+            if (isDigit(schematic[rowIdx][colIdx])) {
+                partDigits.push(schematic[rowIdx][colIdx])
+                if (hasAdjacentSymbol(schematic, rowIdx, colIdx)) isValidPart = true
+            } else {
+                const part = partDigits.join('')
+                if (isValidPart && isNumber(part)) {
+                    const validPart = toNumber(part)
+                    // Look for adjacent gears, and add to them
+                    const gearsIndex = getAdjacentSimbolsIndexByRowRange(schematic, rowIdx, colIdx - 1 - (part.length - 1), colIdx - 1, isGearSimbol)
+                    gearsIndex.map(gearIdx => {
+                        if (!gearsNumbers[gearIdx]) gearsNumbers[gearIdx] = [] //init gear numbers array
+                        gearsNumbers[gearIdx].push(validPart)
+                    })
+                }
+                partDigits.length = 0 //reset partDigits
+                isValidPart = false
+            }
+        })
+        //end of line
+        const colIdx = row.length
+        const part = partDigits.join('')
+        if (isValidPart && isNumber(part)) {
+            const validPart = toNumber(part)
+            // Look for adjacent gears, and add to them
+            const gearsIndex = getAdjacentSimbolsIndexByRowRange(schematic, rowIdx, colIdx - 1 - (part.length - 1), colIdx - 1, isGearSimbol)
+            gearsIndex.map(gearIdx => {
+                if (!gearsNumbers[gearIdx]) gearsNumbers[gearIdx] = [] //init gear numbers array
+                gearsNumbers[gearIdx].push(validPart)
+            })
+        }
+        partDigits.length = 0 //reset partDigits
+        isValidPart = false
+    })
+
+    return Object.keys(gearsNumbers).filter(gearIdx => gearsNumbers[gearIdx].length === 2)
+        .reduce((counter, gearIdx) => {
+            const gearRatio = gearsNumbers[gearIdx][0] * gearsNumbers[gearIdx][1]
+            return counter + gearRatio
+        }, 0)
 }
--- day3/index.test.js
+++ day3/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { sumPartNumbers } from 'day3'
+import { sumPartNumbers, sumGearRatios } from 'day3'
 import path from 'path'
 
 test('Test sumPartNumbers sample input', () => {
@@ -14,4 +14,18 @@ test('Test sumPartNumbers input', () => {
     const expectedOutput = 551094
 
     expect(sumPartNumbers(inputFile)).toBe(expectedOutput)
+})
+
+test('Test sumGearRatios sample input', () => {
+    const inputFile = path.join('day3', 'input.sample.txt')
+    const expectedOutput = 467835
+
+    expect(sumGearRatios(inputFile)).toBe(expectedOutput)
+})
+
+test('Test sumGearRatios input', () => {
+    const inputFile = path.join('day3', 'input.txt')
+    const expectedOutput = 80179647
+
+    expect(sumGearRatios(inputFile)).toBe(expectedOutput)
 })
